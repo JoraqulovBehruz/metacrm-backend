a9c6313a85a6462cf08e0ef67a926b85363b8801
Jo'raqulov Behruz <behruzbek8414@gmail.com>
2026-06-19 16:44:22 +0500

    Bazaga push data base

--- Program.cs
+++ Program.cs
@@ -9,12 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); ;
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // JWT
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 var secretKey = jwtSettings["SecretKey"]!;
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -37,13 +36,13 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
     {
         policy.WithOrigins(
-    "http://localhost:4200",
-    "http://localhost:52196",
-    "https://metacrm-frontend.vercel.app",
-    "https://metacrm-frontend-7x6n44yxe-behruzbek8214s-projects.vercel.app"
-)
-                .AllowAnyHeader()
-              .AllowAnyMethod();
+            "http://localhost:4200",
+            "http://localhost:52196",
+            "https://metacrm-frontend.vercel.app",
+            "https://metacrm-frontend-7x6n44yxe-behruzbek8214s-projects.vercel.app"
+        )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
     });
 });
 
@@ -53,20 +52,19 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Database yaratish
+// Database qayta yaratish
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
 }
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 app.UseCors("AllowAngular");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
